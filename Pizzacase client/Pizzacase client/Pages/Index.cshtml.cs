b4c89d6dfd29b7ecebf00ef3ee6c6ebc2d0e0484
Gewoon-Dennis <geertsma.dennis@gmail.com>
2023-03-21 13:23:32 +0100

    Huts

--- Pizzacase client/Pizzacase client/Pages/Index.cshtml.cs
+++ Pizzacase client/Pizzacase client/Pages/Index.cshtml.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Sockets;
+using Pizzacase_client.Connection;
 
-namespace Pizzacase_client.Pages
-{
+namespace Pizzacase_client.Pages;
+
     public class IndexModel : PageModel
     {
+        ConnectionServer server = ConnectionServer.GetInstance();
+    
+
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty ] public string Name { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -16,5 +24,11 @@ namespace Pizzacase_client.Pages
         {
 
         }
+
+        public void OnPostKlik() 
+        {
+            string klik = "\n-------------\nJansen\nNieuwestad 14\n8901 PM Leeuwarden\nCalzone\n2\n0\nDiavolo\n1\n1\nMozzarella\n05/12/2022 18:32\n-------------";
+            string test = server.SendMessage(klik);
+            Name = test;
+        }
     }
-}
