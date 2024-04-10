668be82605630f192631c92df49aafc7bfb65bb7
Gewoon-Dennis <geertsma.dennis@gmail.com>
2024-04-10 16:31:15 +0200

    Message Sender en Basic message ontvanger

---
+++ PizzacaseServerSite/ServerListening/ListenerUDP.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace PizzacaseServerSite.ServerListening
+{
+    public class ListenerUDP
+    {
+
+        public void StartUdpServer()
+        {
+            int port = 8080;
+            string ipAddress = "127.0.0.1";
+
+            UdpClient udpListener = new UdpClient(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+            Console.WriteLine("UDP server listening on " + ipAddress + ":" + port);
+
+            try
+            {
+                while (true)
+                {
+                    IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receivedBytes = udpListener.Receive(ref remoteEndPoint);
+                    string receivedMessage = Encoding.UTF8.GetString(receivedBytes);
+
+                    Console.WriteLine("Received message: " + receivedMessage);
+
+                    // Send a response back to the client
+                    string responseMessage = "Message received successfully!";
+                    byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+                    udpListener.Send(responseData, responseData.Length, remoteEndPoint);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                udpListener.Close();
+            }
+        }
+    
+    }
+}
